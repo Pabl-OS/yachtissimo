05dec84dcdfe33c5d3316f6de3176ce8956530d6
Pabl-OS <pablito99sant@gmail.com>
2021-10-05 17:02:49 -0500

    feature maquetar plus component -> YACHT-10

--- src/app/web/bareboats/bareboats.module.ts
+++ src/app/web/bareboats/bareboats.module.ts
@@ -7,16 +7,18 @@ import { FilterComponent } from './components/filter/filter.component';
 import { FilterAdvanceComponent } from './components/filter-advance/filter-advance.component';
 import { BoatsComponent } from './components/boats/boats.component';
 import { BooksComponent } from './components/books/books.component';
+import { PlusComponent } from './components/plus/plus.component';
 
 
 
 @NgModule({
   declarations: [
     BareboatsComponent,
+    BoatsComponent,
+    BooksComponent,
     FilterComponent,
     FilterAdvanceComponent,
-    BoatsComponent,
-    BooksComponent
+    PlusComponent
   ],
   schemas: [
     CUSTOM_ELEMENTS_SCHEMA
