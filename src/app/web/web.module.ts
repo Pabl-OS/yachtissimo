dcc199ca2c509079282af7ceffcebda9c9e37739
Pabl-OS <pablito99sant@gmail.com>
2021-10-07 16:32:56 -0500

    feature Maquetar footer component -> YACHT-12

--- src/app/web/web.module.ts
+++ src/app/web/web.module.ts
@@ -4,13 +4,15 @@ import { CommonModule } from '@angular/common';
 import { WebRoutingModule } from './web-routing.module';
 import { WebComponent } from './web.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 // import { HeaderComponent } from './shared/header/header.component';
 
 
 @NgModule({
   declarations: [
     WebComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
   ],
 
 
