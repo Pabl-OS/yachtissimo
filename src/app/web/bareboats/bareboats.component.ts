363cbc35582595b1b3284c890f28c62d2ad2dd93
Pablo <55471150+Pabl-OS@users.noreply.github.com>
2021-10-19 20:38:16 -0500

    FIX: Reparar responsividad de iconos en cards -> YACHT-31

--- src/app/web/bareboats/bareboats.component.ts
+++ src/app/web/bareboats/bareboats.component.ts
@@ -8,6 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class BareboatsComponent implements OnInit {
 
   hideFilter:boolean = true;
+
+
+
   constructor() { }
 
   ngOnInit(): void {
